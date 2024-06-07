995dbb9903856961250589eb3bf7b683b94db681
Martin Boros <mboros04@gmail.com>
2024-06-07 17:06:04 +0000

    progress hw1

--- exercises/hw2/matrix_mul_shared.cu
+++ exercises/hw2/matrix_mul_shared.cu
@@ -37,15 +37,15 @@ __global__ void mmul(const float *A, const float *B, float *C, int ds) {
     for (int i = 0; i < ds/block_size; i++) {
 
       // Load data into shared memory
-      As[threadIdx.y][threadIdx.x] = A[FIXME];
-      Bs[threadIdx.y][threadIdx.x] = B[FIXME];
+      As[threadIdx.y][threadIdx.x] = A[idx];
+      Bs[threadIdx.y][threadIdx.x] = B[idx];
 
       // Synchronize
       __syncthreads();
 
       // Keep track of the running sum
       for (int k = 0; k < block_size; k++)
-      	temp += As[FIXME][FIXME] * Bs[FIXME][FIXME]; // dot product of row and column
+      	temp += As[idy][idx] * Bs[idy][idx]; // dot product of row and column
       __syncthreads();
 
     }
